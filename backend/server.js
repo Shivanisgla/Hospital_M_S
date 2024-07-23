9ffacccad19c0fb414700f15b149218572e8a08b
KRISHkg31 <krish.gupta_cs.da21@gla.ac.in>
2024-07-23 18:00:14 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
-import app from "./app.js";
-import cloudinary from "cloudinary";
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app.js';
+import cloudinary from 'cloudinary';
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+const port = process.env.PORT || 4000; // Default to 4000 if PORT is not defined in .env
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
